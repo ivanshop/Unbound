47f0af4b2408e8e77d083c6f18e69690b2729f32
ivancho <ivan.patricio.quispe@gmail.com>
2024-11-10 17:39:03 -0400

    Initial commit

---
+++ Unbound.StaffControl.Desktop/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Unbound.StaffControl.Desktop;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
